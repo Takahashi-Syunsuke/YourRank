4f0cbe30f9755de472cb928fed7d5907db24bf84
Takahashi-Syunsuke <58886682+Takahashi-Syunsuke@users.noreply.github.com>
2020-02-01 18:06:47 +0900

    before-gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "ranks#index"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show]
   resources :ranks, only: [:index, :show, :new, :create]
   resources :tags, only: [:new, :create]
 end
