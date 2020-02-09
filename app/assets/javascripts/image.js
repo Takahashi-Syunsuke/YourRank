a55f74dac35b2f83a553c9429d711840a2c6bc94
Takahashi-Syunsuke <58886682+Takahashi-Syunsuke@users.noreply.github.com>
2020-02-10 03:19:51 +0900

    add-imge-by-jquery

---
+++ app/assets/javascripts/image.js
@@ -0,0 +1,141 @@
+$(function(){
+  $('#image-rank-1').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".form__first__image-check__background#rank-1");
+        t = this;
+
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  height: "190px",
+                  width: "auto",
+                  class: "preview-1",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
+
+$(function(){
+  $('#image-rank-2').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".form__first__image-check__background#rank-2");
+        t = this;
+
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  height: "190px",
+                  width: "auto",
+                  class: "preview-2",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
+
+$(function(){
+  $('#image-rank-3').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".form__first__image-check__background#rank-3");
+        t = this;
+
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  height: "190px",
+                  width: "auto",
+                  class: "preview-3",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
+
+$(function(){
+  $("#delete-rank-1").click( function() {
+    $(".preview-1").remove();
+    $('input#rank_image_1').val('')
+    return false;
+  });
+});
+
+$(function(){
+  $("#delete-rank-2").click( function() {
+    $(".preview-2").remove();
+    $('input#rank_image_2').val('')
+    return false;
+  });
+});
+
+$(function(){
+  $("#delete-rank-3").click( function() {
+    $(".preview-3").remove();
+    $('input#rank_image_3').val('')
+    return false;
+  });
+});
+
+$(function(){
+  $('form').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".icon");
+        t = this;
+
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  class: "preview",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
+
+$(function(){
+  $('.field-icon-reset').click( function() {
+    $(".preview").remove();
+    $('input[type=file]').val('')
+    return false;
+  });
+});
