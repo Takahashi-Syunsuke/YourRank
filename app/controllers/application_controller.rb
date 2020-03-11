d027f603196e75f15340ed1caa3754d56d098cac
Takahashi-Syunsuke <58886682+Takahashi-Syunsuke@users.noreply.github.com>
2020-03-11 18:24:40 +0900

    basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile, :image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile, :image])
   end
+
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:basic_auth_user] && password == Rails.application.credentials[:basic_auth][:basic_auth_password]
+    end
+  end
+
 end
